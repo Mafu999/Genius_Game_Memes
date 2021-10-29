affc502ee2ad7668859861b68bb25439f79cff2a
Pedro <mafuzao@outlook.com>
2021-10-28 21:06:27 -0300

    Criando função que irá identificar a cor selecionada e dar um aspecto de botão pressionado

---
+++ script.js
@@ -0,0 +1,57 @@
+let order = [];
+let clickedOrder = [];
+let score = 0;
+
+//0 green
+//1 blue
+//2 red
+//3 yellow
+
+const green = document.querySelector('.green');
+const blue = document.querySelector('.blue');
+const red = document.querySelector('.red');
+const yellow = document.querySelector('.yellow');
+
+let randomOrder = ()=>{
+    let sequence = Math.round(Math.random() * 4);
+    order = sequence;
+    clickedOrder = [];
+
+    for(let i = 0; i < order.length; i++){
+        let elementColor = createColorElement(order[i]);
+        lightColor(elementColor, Number(i) + 1);
+    }
+}
+
+let lightColor = (element, number) => {
+    number = number * 500;
+    setTimeout(() =>{
+        element.classList.add('selected');
+    }, number - 250);
+    setTimeout(() => {
+        element.classList.remove('selected');
+    }, number - 250);
+}
+
+let checkingOrder = () =>{
+    for(let i in clickedOrder) {
+        if(clickedOrder[i] != order[i]) {
+            gameOver();
+            break;
+        }
+    }
+    if(clickedOrder.length == order.length) {
+        alert(`Pontuação: ${score}\nVocê acertou! Next Level... `);
+        nextLevel();
+    }
+}
+
+let click = (color) => {
+    clickedOrder[clickedOrder.length] = color;
+    createColorElement(color).classList.add('selected');
+
+    setTimeout(() => {
+        createColorElement(color).classList.remove('selected');
+        checkingOrder()
+    },250);
+}
